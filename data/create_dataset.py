0a79c9f60170b185b806df81bab35f873563c5be
Zheyu-In-The-Git <85062386+Zheyu-In-The-Git@users.noreply.github.com>
2023-04-13 14:59:15 +0800

    4月13日 生成Celeba数据集的人脸匹配和非匹配文件

--- data/create_dataset.py
+++ data/create_dataset.py
@@ -5,19 +5,6 @@ import numpy as np
 import numpy.random
 import pandas as pd
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-from torch.utils.data import Dataset
-from torch.utils.data import DataLoader
-
-from torchvision import datasets
-from torchvision import transforms
-
-import matplotlib.pyplot as plt
-from PIL import Image
-
 from functools import partial
 
 numpy.random.seed(83)
@@ -31,6 +18,8 @@ list_eval_partition_data = pd.read_table(fn('list_eval_partition.txt'), delim_wh
 
 # 身份信息对应的图片信息
 identity_CelebA = pd.read_table(fn('identity_CelebA.txt'), delim_whitespace=True, header=None, index_col=None, names = ['img','id'])
+
+# id 是img_x的id
 
 '''--训练集效果---'''
 # 训练集作出img partition id 的效果
@@ -51,9 +40,10 @@ train_data_img_identity_independence = train_data_img_identity.drop_duplicates('
 identity_CelebA_drop_duplicates = identity_CelebA.drop_duplicates('id', keep = 'first')
 # print(identity_CelebA_drop_duplicates)
 
-
-'''--验证集效果---'''
+'''
+# 验证集效果
 # 验证集作出 img partition id的效果
+
 validation_data_img_partition = list_eval_partition_data.loc[list_eval_partition_data['partition'] == 1]
 # print(validation_data_img_partition)
 
@@ -94,11 +84,14 @@ validation_data_img_partition_identity_differentimg_nonmatch.rename(columns={'im
 celeba_facerecognition_validation_dataset = pd.concat([validation_data_img_partition_identity_sameimg_match, validation_data_img_partition_identity_differentimg_nonmatch])
 col_list = ['img_x', 'img_y', 'match', 'id', 'partition']
 celeba_facerecognition_validation_dataset = celeba_facerecognition_validation_dataset[col_list]
-print(celeba_facerecognition_validation_dataset)
+# print(celeba_facerecognition_validation_dataset)
 # celeba_facerecognition_validation_dataset.to_json('celeba_facerecognition_validation_dataset.txt', orient='records')
-# celeba_facerecognition_validation_dataset.to_csv("celeba_facerecognition_validation_dataset.csv", encoding="utf_8_sig")
+# celeba_facerecognition_validation_dataset.to_csv("celeba_facerecognition_test_dataset.csv", encoding="utf_8_sig")
 # 用csv吧
+'''
 
+
+identity_CelebA_drop_duplicates_fortest = identity_CelebA.drop_duplicates('id', keep='last')
 
 '''--测试集效果---'''
 # 测试集作出 img partition id 的效果
@@ -107,6 +100,35 @@ test_data_img_partition_identity = pd.merge(test_data_img_partition, identity_Ce
 # print(test_data_img_partition_identity)
 
 
+# 测试集匹配效果
+test_data_img_partition_identity_sameimg = pd.merge(test_data_img_partition_identity, identity_CelebA_drop_duplicates_fortest, on='id')
+# print(test_data_img_partition_identity_sameimg)
+
+test_data_img_partition_identity_sameimg.insert(loc = 4, column='match', value=np.ones(test_data_img_partition_identity_sameimg.shape[0]))
+
+test_data_img_partition_identity_sameimg_match = test_data_img_partition_identity_sameimg.copy()
+# print('测试集匹配的部分')
+# print(test_data_img_partition_identity_sameimg_match)
 
 
+# 测试集非匹配效果
+temp = test_data_img_partition_identity.copy()
+random_img = test_data_img_partition_identity['img'].sample(n=test_data_img_partition_identity.shape[0]).values
+temp.insert(loc = 3, column='img_y', value = random_img)
+test_data_img_partition_identity_differentimg = temp.copy()
+# print(test_data_img_partition_identity_differentimg)
+
+test_data_img_partition_identity_differentimg_nonmatch = test_data_img_partition_identity_differentimg.copy()
+test_data_img_partition_identity_differentimg_nonmatch.insert(loc = 4, column='match', value=np.zeros(test_data_img_partition_identity_sameimg.shape[0]))
+test_data_img_partition_identity_differentimg_nonmatch.rename(columns={'img':'img_x'}, inplace=True)
+# print(test_data_img_partition_identity_differentimg_nonmatch)
+
+# 合并两个表
+
+celeba_facerecognition_test_dataset = pd.concat([test_data_img_partition_identity_sameimg_match, test_data_img_partition_identity_differentimg_nonmatch])
+col_list = ['img_x', 'img_y', 'match', 'id', 'partition']
+celeba_facerecognition_test_dataset = celeba_facerecognition_test_dataset[col_list]
+print(celeba_facerecognition_test_dataset)
+# celeba_facerecognition_test_dataset.to_csv("celeba_facerecognition_test_dataset.csv", encoding="utf_8_sig")
+# 用csv吧
 
