0bf5f26e9284b1313bf7b342de80e1836849f7bb
Zheyu-In-The-Git <85062386+Zheyu-In-The-Git@users.noreply.github.com>
2023-04-21 14:52:29 +0800

    4月21日 尝试训练

--- arcface_resnet50.py
+++ arcface_resnet50.py
@@ -1,10 +1,6 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import numpy as np
-from torch.utils.data import DataLoader
-from torchvision.datasets import ImageFolder
-from torchvision.transforms import transforms
+import torchmetrics
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
 import torch.optim as optim
@@ -14,16 +10,10 @@ from data import CelebaInterface
 from pytorch_lightning.loggers import TensorBoardLogger
 from argparse import ArgumentParser
 from model import ResNet50, ArcMarginProduct
+from torchmetrics.functional import pairwise_cosine_similarity
+from torchmetrics.functional import mean_squared_error
 
 pl.seed_everything(83)
-
-
-def batch_misclass_rate(y_pred, y_true):
-    return np.sum(y_pred != y_true) / len(y_true)
-
-
-def batch_accuracy(y_pred, y_true):
-    return np.sum(y_pred == y_true) / len(y_true)
 
 
 
@@ -36,10 +26,16 @@ class ArcfaceResnet50(pl.LightningModule):
         self.save_hyperparameters()
         self.criterion = nn.CrossEntropyLoss()
 
+        # 使用一些度量
+        # 预测准确度
+        self.train_acc = torchmetrics.Accuracy(task='Multiclass', num_classes=10177)
+        self.valid_acc = torchmetrics.Accuracy(task='Multiclass', num_classes=10177)
+        self.roc = torchmetrics.ROC(task='binary')
+
     def forward(self, x, u):
         z = self.resnet50(x)
         output = self.arc_margin_product(z, u)
-        return output
+        return output, z
 
     def configure_optimizers(self):
         b1 = 0.5
@@ -47,45 +43,69 @@ class ArcfaceResnet50(pl.LightningModule):
         optim_train = optim.Adam(self.parameters(), lr=0.001, betas=(b1, b2))
         return optim_train
 
-    def get_stats(self, decoded, labels):
-        preds = torch.argmax(decoded, 1).cpu().detach().numpy()
-        accuracy = batch_accuracy(preds, labels.cpu().detach().numpy())
-        misclass_rate = batch_misclass_rate(preds, labels.cpu().detach().numpy())
-        return accuracy, misclass_rate
-
-    def cosin_metric(self, x1, x2):
-        return torch.tensor(np.dot(x1, x2) / (np.linalg.norm(x1) * np.linalg.norm(x2)))
-
+    def calculate_eer(self, metrics, match):
+        fpr, tpr, thresholds = self.roc(metrics, match)
+        eer = 1.0
+        min_dist = 1.0
+        for i in range(len(fpr)):
+            dist = abs(fpr[i] - (1 - tpr[i]))
+            if dist < min_dist:
+                min_dist = dist
+                eer = (fpr[i] + (1 - tpr[i])) / 2
+        return fpr, tpr, thresholds, eer
 
     def training_step(self, batch, batch_idx):
         x, u, _ = batch
-        output = self(x, u)
+        output, _ = self.forward(x, u)
         loss = self.criterion(output, u)
         self.log('train_loss', loss, on_step=True, on_epoch=True)
-
-        acc, miss_rate = self.get_stats(output, u)
-        self.log('train_acc', acc, on_step = True, on_epoch=True)
-        self.log('train_miss_rate', miss_rate, on_step=True, on_epoch=True)
-
+        self.train_acc(output, u)
+        self.log('train_acc', self.train_acc, on_step=True, on_epoch=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        img_x, img_y, match = batch
+        x, u, _ = batch
+        output, _ = self.forward(x, u)
+        self.valid_acc(output, u)
+        self.log('valid_acc', self.valid_acc, on_step=True, on_epoch=True)
 
-
-        self.log("val_acc", acc)
+        loss = self.criterion(output, u)
+        self.log('valid_loss', loss, on_step=True, on_epoch=True)
 
     def test_step(self, batch, batch_idx):
-        img_x, img_y, match = batch
+        img_1, img_2, match = batch
+        z_1 = self.resnet50(img_1)
+        z_2 = self.resnet50(img_2)
+        cos = pairwise_cosine_similarity(z_1, z_2)
+        mse = mean_squared_error(z_1, z_2)
+        return {'cos':cos, 'mse':mse, 'match':match}
+
+    def test_epoch_end(self, outputs):
+        cos = torch.cat([x['cos'] for x in outputs], dim=0)
+        mse = torch.cat([x['mse'] for x in outputs], dim=0)
+        match = torch.cat([x['match'] for x in outputs], dim=0)
+
+        fpr_cos, tpr_cos, thresholds_cos, eer_cos = self.calculate_eer(cos, match)
+        fpr_mse, tpr_mse, thresholds_mse, eer_mse = self.calculate_eer(mse, match)
+
+        tensorboard_cos = {'fpr_cos':fpr_cos,
+                           'tpr_cos':tpr_cos,
+                           'thresholds_coss':thresholds_cos,
+                           'eer_cos':eer_cos}
+        tensorboard_mse = {
+            'fpr_mse':fpr_mse,
+            'tpr_mse':tpr_mse,
+            'thresholds_mse':thresholds_mse,
+            'eer_mse':eer_mse
+        }
+
+        self.log_dict(tensorboard_cos)
+        self.log_dict(tensorboard_mse)
 
 
-        acc = (u == preds).float().mean()
-        self.log("test_acc", acc)
 
 
-
-
-CHECKPOINT_PATH = os.environ.get('PATH_CHECKPOINT', 'lightning_logs/face_recognizer_resnet50_latent1024/checkpoints/')
+CHECKPOINT_PATH = os.environ.get('PATH_CHECKPOINT', 'lightning_logs/arcface_recognizer_resnet50_latent1024/checkpoints/')
 os.makedirs(CHECKPOINT_PATH, exist_ok=True)
 
 def main(model_name, Resume, save_name=None):
@@ -113,8 +133,8 @@ def main(model_name, Resume, save_name=None):
     trainer = pl.Trainer(
         callbacks=[
             ModelCheckpoint(
-                mode="max",
-                monitor="val_acc",
+                mode="min",
+                monitor="valid_loss_epoch",
                 dirpath=os.path.join(CHECKPOINT_PATH, 'saved_model', save_name)
             ),  # Save the best checkpoint based on the maximum val_acc recorded. Saves only weights and not optimizer
             LearningRateMonitor("epoch"),
@@ -123,14 +143,15 @@ def main(model_name, Resume, save_name=None):
         default_root_dir=os.path.join(CHECKPOINT_PATH, 'saved_model', save_name),  # Where to save models
         accelerator="auto",
         devices=1,
-        max_epochs=120,
-        min_epochs=100,
+        max_epochs=100,
+        min_epochs=50,
         logger=logger,
         log_every_n_steps=10,
         precision=32,
         enable_checkpointing=True,
         check_val_every_n_epoch=10,
-        fast_dev_run=False
+        fast_dev_run=False,
+        reload_dataloaders_every_n_epochs=1
     )
     trainer.logger._log_graph = True  # If True, we plot the computation graph in tensorboard
     trainer.logger._default_hp_metric = None  # Optional logging argument that we don't need
@@ -141,7 +162,7 @@ def main(model_name, Resume, save_name=None):
         os.makedirs(resume_checkpoint_dir, exist_ok=True)
         resume_checkpoint_path = os.path.join(resume_checkpoint_dir, save_name)
         print('Found pretrained model at ' + resume_checkpoint_path + ', loading ... ')  # 重新加载
-        model = FaceRecognizer()
+        model = ArcfaceResnet50()
         trainer.fit(model, data_module, ckpt_path=resume_checkpoint_path)
 
     else:
@@ -149,7 +170,7 @@ def main(model_name, Resume, save_name=None):
         os.makedirs(resume_checkpoint_dir, exist_ok=True)
         resume_checkpoint_path = os.path.join(resume_checkpoint_dir, save_name)
         print('Model will be created')
-        model = FaceRecognizer()
+        model = ArcfaceResnet50()
         trainer.fit(model, data_module)
         trainer.save_checkpoint(resume_checkpoint_path)
 
