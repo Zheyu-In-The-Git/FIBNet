a778af43c738e2b23b576b6139bc87ac614d0cfe
zheyu_chen-in-the-gitee <chenzheyu0989@163.com>
2023-05-16 20:05:20 +0800

    5月15日 在Win11 系统训练bottleneck

--- main.py
+++ main.py
@@ -171,7 +171,7 @@ if __name__ == '__main__':
     parser.add_argument('--beta', default=0.01, type=float)
     parser.add_argument('--batch_size', default=64, type=int)
     parser.add_argument('--max_epochs', default=150, type = int)
-    parser.add_argument('--min_epochs', default=70, type=int)
+    parser.add_argument('--min_epochs', default=100, type=int)
 
 
     # 日志参数
