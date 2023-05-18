b308bac1b025d47ea5ffca7697d55e171be30441
zheyu_chen-in-the-gitee <chenzheyu0989@163.com>
2023-05-18 14:40:28 +0800

    5月18日 改变了草稿

--- draft.py
+++ draft.py
@@ -281,6 +281,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model = torch.load('lightning_logs/model_ir_se50.pth', map_location=device)
 print(model)
 '''
+'''
 
 import pickle
 import torch
@@ -317,5 +318,62 @@ print(model)
 
 model.fc = nn.Linear(2048, 512)
 print(model)
+'''
 
 
+
+'''
+
+import torch
+import torch.nn as nn
+import math
+
+class Encoder(nn.Module):
+    def __init__(self, latent_dim, arcface_model):
+
+        super(Encoder, self).__init__()
+
+        # 将arcface的resnet部分中的layer[1],[2],[3]冻结住
+        self.arcface_model_resnet50 = arcface_model.resnet50
+        #for name, param in self.arcface_model_resnet50.named_parameters():
+        #    param.requires_grad_(False)
+
+        #for param in self.arcface_model_resnet50.layer4.parameters():
+        #    param.requires_grad = True
+
+        #for param in self.arcface_model_resnet50.fc.parameters():
+        #    param.requires_grad = True
+
+        in_features = self.arcface_model_resnet50.fc.in_features
+
+        self.arcface_model_resnet50.fc = nn.Linear(in_features, 512)
+
+        self.batchnorm512 = nn.BatchNorm1d(512)
+
+        self.leakyrelu = nn.LeakyReLU(negative_slope=1e-2, inplace=True)
+
+        self.mu_fc = nn.Linear(512, latent_dim)
+
+        self.log_var_fc = nn.Linear(512, latent_dim)
+
+    def forward(self, x): # 输入的是表征
+        x = self.arcface_model_resnet50(x)
+        x = self.batchnorm512(x)
+        x = self.leakyrelu(x)
+
+        mu = self.mu_fc(x)
+        log_var = self.log_var_fc(x)
+        return mu, log_var
+
+
+if __name__ == '__main__':
+
+     from arcface_resnet50 import *
+     arcface_resnet50_net = ArcfaceResnet50(in_features=512, out_features=10177, s=64.0, m=0.50)
+     #model = arcface_resnet50_net.load_from_checkpoint('/Users/xiaozhe/PycharmProjects/Bottleneck_Nets/lightning_logs/arcface_recognizer_resnet50_latent512/checkpoints/saved_model/face_recognition_resnet50/epoch=140-step=279350.ckpt')
+     net = Encoder(latent_dim=512, arcface_model=arcface_resnet50_net)
+     print(net)
+     x = torch.randn(5, 3, 224, 224)
+     mu, log_var = net(x)
+     print(mu, log_var)
+'''
