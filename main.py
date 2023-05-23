c62f4d451ec3b7f5f88e6e360708c0e08be8b000
zheyu_chen-in-the-gitee <chenzheyu0989@163.com>
2023-05-23 16:59:28 +0800

    5月23日 取消数据增强，改用原始方案，正在训练 bottleneck beta=0.0001

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ def main(args):
         callbacks=load_callbacks(load_path),
         logger=logger,
         log_every_n_steps=10,
-        precision=32,
+        precision=16,
         enable_checkpointing=True,
         accelerator='gpu',
         devices=1,
@@ -143,10 +143,10 @@ if __name__ == '__main__':
     # Restart Control
     parser.add_argument('--load_best', action='store_true')
     parser.add_argument('--load_dir', default = CHECKPOINT_PATH, type=str, help = 'The root directory of checkpoints.')
-    parser.add_argument('--load_ver', default='bottleneck_experiment_latent512_beta0.00001', type=str, help = '训练和加载模型的命名 采用')
+    parser.add_argument('--load_ver', default='bottleneck_experiment_latent512_beta0.0001', type=str, help = '训练和加载模型的命名 采用')
     parser.add_argument('--load_v_num', default = 1, type=int)
     parser.add_argument('--RESUME', default=False, type=bool, help = '是否需要重载模型')
-    parser.add_argument('--ckpt_name', default='bottleneck_experiment_latent512_beta0.001.ckpt', type = str )
+    parser.add_argument('--ckpt_name', default='bottleneck_experiment_latent512_beta0.0001.ckpt', type = str )
     parser.add_argument('--arcface_resnet50_path', default=r'lightning_logs/arcface_recognizer_resnet50_latent512/checkpoints/saved_model/face_recognition_resnet50/epoch=48-step=95800.ckpt')
 
 
@@ -162,14 +162,14 @@ if __name__ == '__main__':
     # 数据集参数设置
     parser.add_argument('--dataset', default='celeba_data', type=str)
     parser.add_argument('--data_dir', default=DATASET_PATH, type=str)
-    parser.add_argument('--num_workers', default=1, type=int)
+    parser.add_argument('--num_workers', default=3, type=int)
     parser.add_argument('--sensitive_attr', default='Male', type=str)
-    parser.add_argument('--pin_memory', default = False)
+    parser.add_argument('--pin_memory', default = True)
 
     # bottleneck_nets的参数
     parser.add_argument('--model_name', default='bottleneck', type=str)
     parser.add_argument('--beta', default=0.0001, type=float)
-    parser.add_argument('--batch_size', default=128, type=int)
+    parser.add_argument('--batch_size', default=64, type=int)
     parser.add_argument('--max_epochs', default=150, type = int)
     parser.add_argument('--min_epochs', default=100, type=int)
 
