257215f7bbdf008feab55734f629dc0292347bb2
zheyu_chen-in-the-gitee <chenzheyu0989@163.com>
2023-06-04 20:19:03 +0800

    6月4日 重新训练 beta 0.00001

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ if __name__ == '__main__':
     # tensorboard记录
     LOG_PATH = os.environ.get('LOG_PATH', '\lightning_logs')
     # 模型加载与命名
-    VERSION = 'bottleneck_experiment_latent_new_512_beta1.0' # 采用非预训练模型
+    VERSION = 'bottleneck_experiment_latent_new_512_beta0.00001' # 采用非预训练模型
     CHECKPOINT_PATH = os.environ.get('PATH_CHECKPOINT', 'lightning_logs/' + VERSION + '/checkpoints/')
 
     ###################
@@ -146,10 +146,10 @@ if __name__ == '__main__':
     # Restart Control
     parser.add_argument('--load_best', action='store_true')
     parser.add_argument('--load_dir', default = CHECKPOINT_PATH, type=str, help = 'The root directory of checkpoints.')
-    parser.add_argument('--load_ver', default='bottleneck_experiment_latent512_beta1.0', type=str, help = '训练和加载模型的命名 采用')
+    parser.add_argument('--load_ver', default='bottleneck_experiment_latent512_beta0.00001', type=str, help = '训练和加载模型的命名 采用')
     parser.add_argument('--load_v_num', default = 1, type=int)
     parser.add_argument('--RESUME', default=False, type=bool, help = '是否需要重载模型')
-    parser.add_argument('--ckpt_name', default='lightning_logs/bottleneck_experiment_latent_new_512_beta1.0/checkpoints/saved_models/last.ckpt', type = str )
+    parser.add_argument('--ckpt_name', default='lightning_logs/bottleneck_experiment_latent_new_512_beta0.00001/checkpoints/saved_models/last.ckpt', type = str )
     parser.add_argument('--arcface_resnet50_path', default=r'lightning_logs/arcface_recognizer_resnet50_latent512/checkpoints/saved_model/face_recognition_resnet50/last.ckpt') # 尝试用last.ckpt
 
 
@@ -171,10 +171,10 @@ if __name__ == '__main__':
 
     # bottleneck_nets的参数
     parser.add_argument('--model_name', default='bottleneck', type=str)
-    parser.add_argument('--beta', default=1.0, type=float)
+    parser.add_argument('--beta', default=0.00001, type=float)
     parser.add_argument('--batch_size', default=64, type=int)
-    parser.add_argument('--max_epochs', default=350, type = int)
-    parser.add_argument('--min_epochs', default=150, type=int)
+    parser.add_argument('--max_epochs', default=90, type = int)
+    parser.add_argument('--min_epochs', default=50, type=int)
 
 
     # 日志参数
