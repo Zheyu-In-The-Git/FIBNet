c9742d3f0a918e8808e941d56f1ec2e69e5e08cd
zheyu_chen-in-the-gitee <chenzheyu0989@163.com>
2023-06-12 20:55:27 +0800

    6月12日  但是 arcface mine test race 还没训练实验完成，需要继续实验 , 正在训练bottleneck race mine lfw

--- experiments/bottleneck_mine_experiments.py
+++ experiments/bottleneck_mine_experiments.py
@@ -92,19 +92,19 @@ def BottleneckMineMain(arcface_model_path, bottleneck_model_path,latent_dim, bet
     '''
 
 
-    '''
+
     
     data_module = LFWInterface(num_workers=2,
                                dataset='lfw',
                                data_dir='D:\datasets\lfw\lfw112',
                                batch_size=256,
                                dim_img=224,
-                               sensitive_attr='Male',
+                               sensitive_attr='White',
                                purpose='attr_extract',
                                pin_memory=False,
                                identity_nums=5749,
                                sensitive_dim=1)
-    '''
+
 
     '''
     
@@ -121,6 +121,7 @@ def BottleneckMineMain(arcface_model_path, bottleneck_model_path,latent_dim, bet
     '''
 
 
+    '''
     
     data_module = CelebaRaceInterface(
         num_workers=2,
@@ -132,6 +133,7 @@ def BottleneckMineMain(arcface_model_path, bottleneck_model_path,latent_dim, bet
         identity_nums=10177,
         pin_memory=False
     )
+    '''
 
 
     '''
@@ -151,7 +153,7 @@ def BottleneckMineMain(arcface_model_path, bottleneck_model_path,latent_dim, bet
 
 
 
-    CHECKPOINT_PATH = os.environ.get('PATH_CHECKPOINT', 'lightning_logs/bottleneck_mine_estimator_celeba_train_race/checkpoints_beta'+str(beta))
+    CHECKPOINT_PATH = os.environ.get('PATH_CHECKPOINT', 'lightning_logs/bottleneck_mine_estimator_lfw_race/checkpoints_beta'+str(beta))
 
     logger = TensorBoardLogger(save_dir=CHECKPOINT_PATH, name='bottleneck_mine_estimator_logger_beta' + str(beta))
 
@@ -176,7 +178,7 @@ def BottleneckMineMain(arcface_model_path, bottleneck_model_path,latent_dim, bet
         accelerator="auto",
         devices=1,
         max_epochs=250,
-        min_epochs=120,
+        min_epochs=200,
         logger=logger,
         log_every_n_steps=10,
         precision=32,
@@ -191,15 +193,15 @@ def BottleneckMineMain(arcface_model_path, bottleneck_model_path,latent_dim, bet
     os.makedirs(resume_checkpoint_dir, exist_ok=True)
     resume_checkpoint_path = os.path.join(resume_checkpoint_dir, save_name)
     print('Model will be created')
-    trainer.fit(bottlenecknetsmineestimator, data_module, ckpt_path=r'C:\Users\40398\PycharmProjects\Bottleneck_Nets\experiments\lightning_logs\bottleneck_mine_estimator_celeba_train_race\checkpoints_beta0.1\saved_model\bottleneck_mine_512_adience\last.ckpt')
+    trainer.fit(bottlenecknetsmineestimator, data_module)
 
 if __name__ == '__main__':
     arcface_model_path = r'C:\Users\40398\PycharmProjects\Bottleneck_Nets\lightning_logs\arcface_recognizer_resnet50_latent512\checkpoints\saved_model\face_recognition_resnet50\last.ckpt'
     #bottleneck_model_path = r'C:\Users\40398\PycharmProjects\Bottleneck_Nets\lightning_logs\bottleneck_experiment_latent_new_512_beta0.0001\checkpoints\saved_models\last.ckpt'
     latent_dim = 512
-    save_name = 'bottleneck_mine_512_adience'
+    save_name = 'bottleneck_mine_512_lfw'
 
-    beta_arr = [0.1]
+    beta_arr = [0.0001, 0.001, 0.01, 0.1, 1.0]
     for beta in beta_arr:
         bottleneck_model_path = r'C:\Users\40398\PycharmProjects\Bottleneck_Nets\lightning_logs\bottleneck_experiment_latent_new_512_beta' + str(beta) + '\checkpoints\saved_models\last.ckpt'
         BottleneckMineMain(arcface_model_path, bottleneck_model_path, latent_dim, beta, save_name)
