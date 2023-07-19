e799f4fb8f8a6948b4cee5f2a131cb92f383c588
Zheyu-In-The-Git <chenzheyu0989@163.com>
2023-07-19 20:01:33 +0800

    7月19日，TSNE

--- T-SNE/data.py
+++ T-SNE/data.py
@@ -90,6 +90,85 @@ class CelebaTSNEExperiment(data.Dataset):
         return x, u, s
 
 
+class CelebaTSNERaceExperiment(data.Dataset):
+    def __init__(self,
+                 dim_img:int,
+                 data_dir:str,
+                 split:str):
+        self.dim_img = dim_img
+        self.data_dir = data_dir
+        self.split = split
+
+        split_map = {
+            'train':0,
+            'valid':1,
+            'test':2,
+            'all':None,
+            'train_63%':'train_63%',
+            'valid_7%':'valid_7%',
+            'test_30%':'test_30%'
+        }
+        split_ = split_map[verify_str_arg(split.lower(), "split",
+                                          ("train", "valid", "test", "all", 'train_valid_70%', 'test_30%'))]
+
+        fn = partial(os.path.join, self.data_dir)
+        imgpath_id = pd.read_table(fn('identity_CelebA.txt'), delim_whitespace=True, header=None, index_col=None, names=['img_path', 'id'])
+
+        imgpath_white = pd.read_csv(fn('celeba_imgpath_race.csv'))
+        imgpath_white = imgpath_white.drop(imgpath_white.columns[0], axis=1)
+        imgpath_race_id = pd.merge(imgpath_white, imgpath_id, on='img_path', how='left')
+
+        print(imgpath_race_id)
+
+        if split_ == 'train_63%':
+            mask = slice(0, 118063, 1)
+            self.trans = transforms.Compose([transforms.Resize(self.dim_img),
+                                             transforms.RandomHorizontalFlip(p=0.5),
+                                             transforms.ToTensor(),
+                                             transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
+            self.dataset = imgpath_race_id[mask]
+
+        elif split_ == 'valid_7%':
+            mask = slice(118063, 131207, 1)
+            self.trans = transforms.Compose([transforms.Resize(self.dim_img),
+                                             transforms.ToTensor(),
+                                             transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
+            self.dataset = imgpath_race_id[mask]
+
+        elif split_ == 'test_30%':
+            mask = slice(131207, 187644, 1)
+            self.trans = transforms.Compose([transforms.Resize(self.dim_img),
+                                             transforms.ToTensor(),
+                                             transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
+            self.dataset = imgpath_race_id[mask]
+
+
+        #print(white_select_condition)
+        white_dataset = self.dataset.where(self.dataset['white'] == 1.0)
+        white_dataset = white_dataset.dropna(axis=0)
+        white_dataset = white_dataset.head(500)
+        #print(white_dataset)
+
+        colored_people = self.dataset.where(self.dataset['white'] == 0.0)
+        colored_people = colored_people.dropna(axis=0)
+        colored_people = colored_people.head(500)
+        #print(colored_people)
+
+        self.dataset_samples = pd.concat([white_dataset, colored_people])
+        print(self.dataset_samples)
+
+
+
+
+
+
+
+
+
+
+
+
+
 class LFWTSNEExperiment(data.Dataset):
     pass
 
@@ -98,4 +177,18 @@ class AdienceTSNEExperiment(data.Dataset):
 
 
 
+if __name__ == '__main__':
+    data_dir = 'D:\celeba'
+    loader = CelebaTSNEExperiment(dim_img=112, data_dir=data_dir, split='test_30%',sensitive_attr='Male')
+    train_loader = DataLoader(loader, batch_size=2)
+    for i, item in enumerate(train_loader):
+        print('i', i)
+        x, u, s = item
+        print(x)
+        print(u)
+        print(s)
+        break
+
+    celeba_race_loader = CelebaTSNERaceExperiment(dim_img=112, data_dir = data_dir, split='test_30%')
+    #celeba_race_test_loader = DataLoader(celeba_race_loader, batch_size=2)
 
