b09b5f260a496ec4a74cbae204e3552183ac1bb7
Zheyu-In-The-Git <85062386+Zheyu-In-The-Git@users.noreply.github.com>
2023-07-23 16:57:26 +0800

    7月22日 t-sne 已经写完了，可以在实验室得到数据了

--- T-SNE/t-sne.py
+++ T-SNE/t-sne.py
@@ -19,112 +19,9 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
-device = torch.device('cuda' if torch.cuda.is_available() else "cpu")  # ******#
+device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
 
 
-
-'''
-gender data
-'''
-
-
-# Celeba数据集的准备
-celeba_dir = 'D:\celeba'
-celeba_dataset = CelebaTSNEExperiment(dim_img=224, data_dir=celeba_dir, sensitive_attr='Male', split='test_30%')
-dataloader = DataLoader(celeba_dataset, batch_size=50, shuffle=True)
-
-
-
-
-
-
-
-
-# 导入模型
-# arcface_resnet50_net = arcface_resnet50.ArcfaceResnet50(in_features=1024, out_features=10177, s=64.0, m=0.50)
-# model = arcface_resnet50_net.load_from_checkpoint(r'C:\Users\40398\PycharmProjects\Bottleneck_Nets\lightning_logs\arcface_recognizer_resnet50_latent1024\checkpoints\saved_model\face_recognition_resnet50\epoch=130-step=259400.ckpt').to(device)
-
-
-# 导入Bottleneck模型
-
-arcface_resnet50_net = arcface_resnet50.ArcfaceResnet50(in_features=512, out_features=10177, s=64.0, m=0.50)
-# arcface = arcface_resnet50_net.load_from_checkpoint(r'C:\Users\40398\PycharmProjects\Bottleneck_Nets\lightning_logs\arcface_recognizer_resnet50_latent512\checkpoints\saved_model\face_recognition_resnet50\last.ckpt')
-arcface = arcface_resnet50_net.load_from_checkpoint(
-    r'C:\Users\Administrator\PycharmProjects\Bottleneck_Nets\lightning_logs\arcface_recognizer_resnet50_latent512\checkpoints\saved_model\face_recognition_resnet50\last.ckpt')
-encoder = Encoder(latent_dim=512, arcface_model=arcface)
-decoder = Decoder(latent_dim=512, identity_nums=10177, s=64.0, m=0.50, easy_margin=False)
-bottlenecknets = BottleneckNets(model_name='bottleneck', encoder=encoder, decoder=decoder, beta=0.1, batch_size=64,
-                                identity_nums=10177)
-# model = bottlenecknets.load_from_checkpoint(r'C:\Users\40398\PycharmProjects\Bottleneck_Nets\lightning_logs\bottleneck_experiment_latent_new_512_beta0.0001\checkpoints\saved_models\last.ckpt', encoder=encoder,decoder=decoder)
-model = bottlenecknets.load_from_checkpoint(
-    r'C:\Users\Administrator\PycharmProjects\Bottleneck_Nets\lightning_logs\bottleneck_experiment_latent_new_512_beta0.01\checkpoints\saved_models\last.ckpt',
-    encoder=encoder, decoder=decoder)
-model.to(device)
-
-# 冻结住网络参数的梯度
-for param in model.parameters():
-    param.requires_grad_(False)
-
-'''
-
-# 让模型进行计算
-Z_data = 0
-S_data = 0
-for i, item in enumerate(dataloader):
-    print('i', i)
-    x, u, s = item
-    U, Z = model(x.to(device),u.to(device))
-    #print(Z.shape)
-    if i == 0:
-        Z_data = Z
-        S_data = s
-    else:
-        Z_data = torch.cat((Z_data, Z), 0)
-        S_data = torch.cat((S_data, s), 0)
-'''
-
-Z_data = 0
-S_data = 0
-for i, item in enumerate(dataloader):
-    print('i', i)
-    x, u, s = item
-    Z, _, _, _ = model(x.to(device), u.to(device))
-    # print(Z.shape)
-    if i == 0:
-        Z_data = Z
-        S_data = s
-    else:
-        Z_data = torch.cat((Z_data, Z), 0)
-        S_data = torch.cat((S_data, s), 0)
-
-Z_data = Z_data.cpu().detach().numpy()
-S_data = S_data.cpu().detach().numpy()
-print(Z_data.shape)
-print(S_data.shape)
-# 进行TSNE计算
-
-
-tsne = TSNE(n_components=2, perplexity=30.0, early_exaggeration=100.0, learning_rate=20.0, n_iter=4300,
-            n_iter_without_progress=300, min_grad_norm=1e-07, metric='cosine', init='pca', verbose=1,
-            random_state=83, method='barnes_hut', angle=0.5)  # 这里metric可以换成cosine 到时候试一下  3*perplexity < nrow(data) - 1
-X_tsne = tsne.fit_transform(Z_data)
-print(X_tsne)
-
-X_tsne_data = np.hstack((X_tsne, S_data))
-np.savez('t-sne.npz',X_tsne_data=X_tsne_data) # 可能就是前500个是1，后500个是0
-# 打开查看用np.load(t-sne.npz)
-
-
-df_tsne = pd.DataFrame(X_tsne_data, columns=['Dim1', 'Dim2', 'gender'])
-df_tsne.loc[df_tsne['gender'] == 0, 'gender'] = 'female'
-df_tsne.loc[df_tsne['gender'] == 1, 'gender'] = 'male'
-
-# markers = {"female": 'y', "male": "b"}
-color = {"female": 'y', "male": "b"}
-
-sns.scatterplot(data=df_tsne, hue='gender', x='Dim1', y='Dim2')  # markers=markers
-# sns.scatterplot(data=df_tsne, hue='gender') # 不打 x轴 和 y轴
-plt.show()
 
 
 
@@ -141,6 +38,9 @@ def TSNEExperiment(model_name, sensitive_attr, dataloader, data_name):
             param.requires_grad_(False)
 
 
+        # 数据加载
+
+
         Z_data = 0
         S_data = 0
         for i, item in enumerate(dataloader):
@@ -155,8 +55,7 @@ def TSNEExperiment(model_name, sensitive_attr, dataloader, data_name):
                 Z_data = torch.cat((Z_data, Z), 0)
                 S_data = torch.cat((S_data, s), 0)
 
-
-
+        # 计算tsne
         tsne = TSNE(n_components=2, perplexity=30.0, early_exaggeration=100.0, learning_rate=20.0, n_iter=4300,
                     n_iter_without_progress=300, min_grad_norm=1e-07, metric='cosine', init='pca', verbose=1,
                     random_state=83, method='barnes_hut',
@@ -165,27 +64,49 @@ def TSNEExperiment(model_name, sensitive_attr, dataloader, data_name):
 
         print(X_tsne)
 
+
+
+
+        # 保存tsne数据，以及对应的s
         X_tsne_data = np.hstack((X_tsne, S_data))
-        np.savez('t-sne.npz', X_tsne_data=X_tsne_data)  # 可能就是前500个是1，后500个是0
+        np.savez('t-sne'+'_'+ sensitive_attr +'_' + model_name +'_'+ data_name +'.npz', X_tsne_data=X_tsne_data)  # 可能就是前500个是1，后500个是0
         # 打开查看用np.load(t-sne.npz)
 
-        df_tsne = pd.DataFrame(X_tsne_data, columns=['Dim1', 'Dim2', 'gender'])
-        df_tsne.loc[df_tsne['gender'] == 0, 'gender'] = 'female'
-        df_tsne.loc[df_tsne['gender'] == 1, 'gender'] = 'male'
 
-        # markers = {"female": 'y', "male": "b"}
-        color = {"female": 'y', "male": "b"}
+        # 查看图片和保存pdf文件
+        if sensitive_attr == 'Male':
 
-        sns.scatterplot(data=df_tsne, hue='gender', x='Dim1', y='Dim2')  # markers=markers
-        # sns.scatterplot(data=df_tsne, hue='gender') # 不打 x轴 和 y轴
-        plt.show()
+            df_tsne = pd.DataFrame(X_tsne_data, columns=['Dim1', 'Dim2', 'sensitive_attr'])
+            df_tsne.loc[df_tsne['sensitive_attr'] == 0, 'sensitive_attr'] = 'female'
+            df_tsne.loc[df_tsne['sensitive_attr'] == 1, 'sensitive_attr'] = 'male'
 
+            sns.scatterplot(data=df_tsne, hue='gender', x='Dim1', y='Dim2')  # markers=markers
+            # sns.scatterplot(data=df_tsne, hue='gender') # 不打 x轴 和 y轴
+
+            plt.title('t-sne'+'_'+ sensitive_attr +'_' + model_name +'_'+ data_name +'.npz')
+            plt.savefig('t-sne'+'_'+ sensitive_attr +'_' + model_name +'_'+ data_name +'.pdf')
+            plt.show()
+            plt.close()
+
+        elif sensitive_attr == 'White':
+
+            df_tsne = pd.DataFrame(X_tsne_data, columns=['Dim1', 'Dim2', 'sensitive_attr'])
+            df_tsne.loc[df_tsne['sensitive_attr'] == 0, 'sensitive_attr'] = 'colored_people'
+            df_tsne.loc[df_tsne['sensitive_attr'] == 1, 'sensitive_attr'] = 'white'
+
+            sns.scatterplot(data=df_tsne, hue='race', x='Dim1', y='Dim2')  # markers=markers
+            # sns.scatterplot(data=df_tsne, hue='gender') # 不打 x轴 和 y轴
+
+            plt.title('t-sne' + '_' + sensitive_attr + '_' + model_name + '_' + data_name + '.npz')
+            plt.savefig('t-sne'+'_'+ sensitive_attr +'_' + model_name +'_'+ data_name +'.pdf')
+            plt.show()
+            plt.close()
 
 
 
 
     #####################################################
-    ########### Bottleneck 0.1 实验部分 ###################
+    ########### Bottleneck 0.1 实验部分 ##################
     #####################################################
     elif model_name == 'Bottleneck_0.1':
         arcface_resnet50_net = arcface_resnet50.ArcfaceResnet50(in_features=512, out_features=10177, s=64.0, m=0.50)
@@ -218,8 +139,6 @@ def TSNEExperiment(model_name, sensitive_attr, dataloader, data_name):
                 S_data = torch.cat((S_data, s), 0)
 
 
-
-
         tsne = TSNE(n_components=2, perplexity=30.0, early_exaggeration=100.0, learning_rate=20.0, n_iter=4300,
                     n_iter_without_progress=300, min_grad_norm=1e-07, metric='cosine', init='pca', verbose=1,
                     random_state=83, method='barnes_hut',
@@ -229,19 +148,123 @@ def TSNEExperiment(model_name, sensitive_attr, dataloader, data_name):
         print(X_tsne)
 
         X_tsne_data = np.hstack((X_tsne, S_data))
-        np.savez('t-sne.npz', X_tsne_data=X_tsne_data)  # 可能就是前500个是1，后500个是0
+        np.savez('t-sne'+'_'+ sensitive_attr +'_' + model_name +'_'+ data_name +'.npz', X_tsne_data=X_tsne_data)  # 可能就是前500个是1，后500个是0
         # 打开查看用np.load(t-sne.npz)
 
-        df_tsne = pd.DataFrame(X_tsne_data, columns=['Dim1', 'Dim2', 'gender'])
-        df_tsne.loc[df_tsne['gender'] == 0, 'gender'] = 'female'
-        df_tsne.loc[df_tsne['gender'] == 1, 'gender'] = 'male'
 
-        # markers = {"female": 'y', "male": "b"}
-        color = {"female": 'y', "male": "b"}
 
-        sns.scatterplot(data=df_tsne, hue='gender', x='Dim1', y='Dim2')  # markers=markers
-        # sns.scatterplot(data=df_tsne, hue='gender') # 不打 x轴 和 y轴
-        plt.show()
+
+
+        # 查看图片和保存pdf文件
+        if sensitive_attr == 'Male':
+
+            df_tsne = pd.DataFrame(X_tsne_data, columns=['Dim1', 'Dim2', 'sensitive_attr'])
+            df_tsne.loc[df_tsne['sensitive_attr'] == 0, 'sensitive_attr'] = 'female'
+            df_tsne.loc[df_tsne['sensitive_attr'] == 1, 'sensitive_attr'] = 'male'
+
+            sns.scatterplot(data=df_tsne, hue='gender', x='Dim1', y='Dim2')  # markers=markers
+            # sns.scatterplot(data=df_tsne, hue='gender') # 不打 x轴 和 y轴
+
+            plt.title('t-sne' + '_' + sensitive_attr + '_' + model_name + '_' + data_name + '.npz')
+            plt.savefig('t-sne' + '_' + sensitive_attr + '_' + model_name + '_' + data_name + '.pdf')
+            plt.show()
+            plt.close()
+
+        elif sensitive_attr == 'White':
+
+            df_tsne = pd.DataFrame(X_tsne_data, columns=['Dim1', 'Dim2', 'sensitive_attr'])
+            df_tsne.loc[df_tsne['sensitive_attr'] == 0, 'sensitive_attr'] = 'colored_people'
+            df_tsne.loc[df_tsne['sensitive_attr'] == 1, 'sensitive_attr'] = 'white'
+
+            sns.scatterplot(data=df_tsne, hue='race', x='Dim1', y='Dim2')  # markers=markers
+            # sns.scatterplot(data=df_tsne, hue='gender') # 不打 x轴 和 y轴
+
+            plt.title('t-sne' + '_' + sensitive_attr + '_' + model_name + '_' + data_name + '.npz')
+            plt.savefig('t-sne' + '_' + sensitive_attr + '_' + model_name + '_' + data_name + '.pdf')
+            plt.show()
+            plt.close()
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    ##########################################################
+    #################   数据集准备部分  #########################
+    ##########################################################
+
+    # 数据集部分
+    celeba_dir = 'D:\datasets\celeba'
+    lfw_dir = 'D:\datasets\lfw'
+    adience_dir = 'D:\datasets\Adience'
+
+    # 性别
+    celeba_gender_dataset = CelebaTSNEExperiment(dim_img=224, data_dir=celeba_dir, sensitive_attr='Male', split='test_30%')
+    celeba_gender_dataloader = DataLoader(celeba_gender_dataset, batch_size=50, shuffle=True)
+
+    lfw_gender_dataset = LFWTSNEExperiment(dim_img=224, data_dir=lfw_dir, sensitive_attr='Male', split='all', img_path_replace=True)
+    lfw_gender_dataloader = DataLoader(lfw_gender_dataset, batch_size=50, shuffle=True)
+
+    adience_gender_dataset = AdienceTSNEGenderExperiment(dim_img=224, data_dir=adience_dir,  sensitive_attr='Male')
+    adience_gender_dataloader = DataLoader(adience_gender_dataset, batch_size=50, shuffle=True)
+
+
+    # 种族
+    celeba_race_dataset = CelebaTSNERaceExperiment(dim_img=224, data_dir=celeba_dir, split='test_30%')
+    celeba_race_dataloader = DataLoader(celeba_race_dataset, batch_size=50, shuffle=True)
+
+    lfw_race_dataset =LFWTSNEExperiment(dim_img=224, data_dir=lfw_dir, sensitive_attr='White', split='all', img_path_replace=True)
+    lfw_race_dataloader = DataLoader(lfw_race_dataset, batch_size=50, shuffle=True)
+
+    adience_race_dataset = AdienceTSNERaceExperiment(dim_img=224, data_dir=adience_dir, identity_nums=10177)
+    adience_race_dataloader = DataLoader(adience_race_dataset, batch_size=50, shuffle=True)
+
+
+
+
+    #############################################################
+    #################   Arcface 实验部分  #########################
+    #############################################################
+
+    # 性别
+    TSNEExperiment(model_name='Arcface', sensitive_attr='Male', dataloader=celeba_gender_dataloader, data_name='celeba')
+    TSNEExperiment(model_name='Arcface', sensitive_attr='Male', dataloader=lfw_gender_dataloader, data_name='lfw')
+    TSNEExperiment(model_name='Arcface', sensitive_attr='Male', dataloader=adience_gender_dataloader, data_name='adience')
+
+    # 种族
+    TSNEExperiment(model_name='Arcface', sensitive_attr='White', dataloader=celeba_race_dataloader, data_name='celeba')
+    TSNEExperiment(model_name='Arcface', sensitive_attr='White', dataloader=lfw_race_dataloader, data_name='celeba')
+    TSNEExperiment(model_name='Arcface', sensitive_attr='White', dataloader=adience_race_dataloader, data_name='celeba')
+
+
+
+
+    #############################################################
+    #################   bottleneck 实验部分  #########################
+    #############################################################
+
+    # 性别
+    TSNEExperiment(model_name='Bottleneck_0.1', sensitive_attr='Male', dataloader=celeba_gender_dataloader, data_name='celeba')
+    TSNEExperiment(model_name='Bottleneck_0.1', sensitive_attr='Male', dataloader=lfw_gender_dataloader, data_name='lfw')
+    TSNEExperiment(model_name='Bottleneck_0.1', sensitive_attr='Male', dataloader=adience_gender_dataloader,data_name='adience')
+
+    # 种族
+    TSNEExperiment(model_name='Bottleneck_0.1', sensitive_attr='White', dataloader=celeba_race_dataloader, data_name='celeba')
+    TSNEExperiment(model_name='Bottleneck_0.1', sensitive_attr='White', dataloader=lfw_race_dataloader, data_name='celeba')
+    TSNEExperiment(model_name='Bottleneck_0.1', sensitive_attr='White', dataloader=adience_race_dataloader, data_name='celeba')
+
+
+
+
+
+
+
+
+
 
 
 
