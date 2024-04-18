322a9b001cb6f4b8b4e720795a22426f09005859
zheyu <chenzheyu0989@163.com>
2024-04-18 14:46:43 +0800

    casia lfw数据写完了，写一下接口吧

--- data/lfw_casia_data.py
+++ data/lfw_casia_data.py
@@ -157,10 +157,121 @@ class LFWCasiaData(data.Dataset):
 
 
 
+class LFWCaisaRecognitionTestPairs(data.Dataset):
+    def __init__(self,
+                 dim_img:int,
+                 lfw_data_dir:str,
+                 casia_data_dir:str):
+        self.dim_img = dim_img
+        self.lfw_data_dir = lfw_data_dir
+        self.casia_data_dir = casia_data_dir
+
+
+        ##############################
+        ###########LFW################
+        ##############################
+        lfw_fn = partial(os.path.join, self.lfw_data_dir)
+        lfw_test_dataset = pd.read_csv(lfw_fn('lfw_test_pair.txt'), delim_whitespace=True, header=None, index_col=None)
+        lfw_test_dataset.columns = ['img_x', 'img_y', 'match']
+        #print(lfw_test_dataset)
+
+        lfw_img_x_local_device_path = []
+        for i in lfw_test_dataset['img_x']:
+            i = lfw_fn('img', i)
+            i = i.replace('/', '\\')
+            lfw_img_x_local_device_path.append(i)
+
+
+        lfw_img_y_local_device_path = []
+        for i in lfw_test_dataset['img_y']:
+            i = lfw_fn('img', i)
+            i = i.replace('/','\\')
+            lfw_img_y_local_device_path.append(i)
+
+
+        lfw_data_img_xy = np.column_stack((lfw_img_x_local_device_path, lfw_img_y_local_device_path))
+
+
+        lfw_data_img_xy_match = np.column_stack((lfw_data_img_xy, lfw_test_dataset['match']))
+
+
+        #lfw最终的数据 两个图像数据对应的匹配值
+        lfw_face_recognition_data = lfw_data_img_xy_match
+        #print(lfw_face_recognition_data)
+
+
+
+
+
+
+        ##############################
+        ###########Casia##############
+        ##############################
+        casia_fn = partial(os.path.join, self.casia_data_dir)
+        casia_test_dataset = pd.read_csv(casia_fn('csv_file', 'casia_face_verify_test_dataset.csv'))
+        casia_test_dataset = casia_test_dataset.drop(casia_test_dataset.columns[0],axis=1)
+        #print(casia_test_dataset)
+
+        casia_img_x_list = []
+        for i in casia_test_dataset['img_x']:
+            i = casia_fn('dataset', i[0:3], i)
+            i = i.replace('bmp', 'jpg')
+            casia_img_x_list.append(i)
+
+
+        casia_img_y_list = []
+        for i in casia_test_dataset['img_y']:
+            i = casia_fn('dataset', i[0:3], i)
+            i = i.replace('bmp', 'jpg')
+            casia_img_y_list.append(i)
+
+        casia_data_img_xy = np.column_stack((casia_img_x_list, casia_img_y_list))
+        casia_data_img_xy_match = np.column_stack((casia_data_img_xy, casia_test_dataset['match']))
+        #print(casia_data_img_xy_match)
+
+
+
+        ###############################
+        ########## LfW+Casia###########
+        ###############################
+        lfw_casia_database_np = np.row_stack((lfw_face_recognition_data, casia_data_img_xy_match))
+        self.lfw_casia_database_pd = pd.DataFrame(lfw_casia_database_np, columns=['img_x','img_y', 'match'])
+        print(self.lfw_casia_database_pd)
+
+
+        self.trans = transforms.Compose([transforms.Resize((self.dim_img, self.dim_img)),
+                                         transforms.ToTensor(),
+                                         transforms.Normalize(mean=[0.5,0.5, 0.5], std=[0.5,0.5,0.5])])
+    def __len__(self):
+        return self.lfw_casia_database_pd.shape[0]
+
+
+    def __getitem__(self, index):
+
+        img_x_path = self.lfw_casia_database_pd['img_x'][index]
+        img_y_path = self.lfw_casia_database_pd['img_y'][index]
+
+        img_x = PIL.Image.open(img_x_path)
+        img_x = self.trans(img_x)
+
+        img_y = PIL.Image.open(img_y_path)
+        img_y = self.trans(img_y)
+
+        match = torch.tensor(float(self.lfw_casia_database_pd['match'][index]))
+
+        return img_x, img_y, match
+
+
+
+
 
 
 
 if __name__ == '__main__':
+
+    ###################################
+    ############# 人脸属性的dataloader###
+    ###################################
     lfw_data_dir = 'E:\datasets\lfw\lfw112'
     casia_data_dir = 'E:\datasets\CASIA-FaceV5\dataset_jpg'
 
@@ -173,11 +284,22 @@ if __name__ == '__main__':
                           split='all')
     train_loader = DataLoader(loader, batch_size=200, shuffle=True)
     for i, item in enumerate(train_loader):
-        print('i', i)
+        #print('i', i)
         x, u, s = item
-        print(x.shape)
-        print(u)
-        print(s)
+        #print(x.shape)
+        #print(u)
+        #print(s)
+        break
+
+    face_loader_data = LFWCaisaRecognitionTestPairs(dim_img=224,
+                                               lfw_data_dir=lfw_data_dir,
+                                               casia_data_dir=casia_data_dir)
+    face_loader = DataLoader(face_loader_data, batch_size=100, shuffle=True)
+    for i, item in enumerate(face_loader):
+        img_x, img_y, match = item
+        print(img_x)
+        print(img_y)
+        print(match)
 
 
 
